71525815933c55459b0807f400a0ddddb078e5d5
geudet <43990448+geudet@users.noreply.github.com>
2019-01-01 10:15:14 +0000

    Add files via upload

---
+++ EX23.py
@@ -0,0 +1,5 @@
+# I use variable a to give a value inserted by user
+a = int(input("Enter an integer number: "))
+
+# using modulo (%) to print the reminder after the division
+print("The remainder result after dividing %d by \7 is: %d " %(a, a%7))
